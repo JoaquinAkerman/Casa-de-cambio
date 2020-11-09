c7c3f9facb64c37cbd1cf4e8e33a04720c696ad7
joaquinluis <joaquin_akerman@hotmail.com>
2020-11-09 14:08:46 -0300

    Implemento cambio de moneda base
    Seleccionando otra moneda podesmos cambiar la base de la casa de cambios

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
+"use strict";
 ///<reference types="jquery"/>
-
 let paginaAPI = "https://api.exchangeratesapi.io/latest";
 
-fetch(paginaAPI)
-	.then((respuesta) => respuesta.json())
-	.then((respuestaJSON) => {
-		$("#fecha").text(`En el día ${respuestaJSON.date}`);
-		$(`#descripcion`).text(`1 ${respuestaJSON.base} es igual a:`);
+function cargarAPI() {
+	fetch(paginaAPI)
+		.then((respuesta) => respuesta.json())
+		.then((respuestaJSON) => {
+			$("#fecha").text(`En el día ${respuestaJSON.date}`);
+			$(`#descripcion`).text(`1 ${respuestaJSON.base} es igual a:`);
 
-		Object.keys(respuestaJSON.rates).forEach((moneda) => {
-			$("#precio").append(
-				`<li class="list-group-item">${respuestaJSON.rates[moneda]}</li>`
-			);
-			$("#moneda").append(`<li class="list-group-item">${moneda}</li>`);
-			$("#lista-monedas").append(`<option value="${moneda}">`);
+			Object.keys(respuestaJSON.rates).forEach((moneda) => {
+				$("#precio").append(
+					`<li class="list-group-item precio">${respuestaJSON.rates[moneda]}</li>`
+				);
+				$("#moneda").append(
+					`<li class="list-group-item moneda">${moneda}</li>`
+				);
+				$("#lista-monedas").append(`<option value="${moneda}">`);
+			});
 
-			function monedaSeleccionada() {
-				nuevaMoneda = $("#nueva-moneda").val();
-				if (nuevaMoneda === moneda) {
-					paginaAPI = `https://api.exchangeratesapi.io/latest?base=${nuevaMoneda}`;
+			function cambiarMonedaBase() {
+				let monedasValidas = Object.keys(respuestaJSON.rates);
+				let nuevaMonedaBase = $("#nueva-moneda").val();
+				for (let i = 0; i < monedasValidas.length; i++) {
+					if (nuevaMonedaBase === monedasValidas[i]) {
+						paginaAPI = `https://api.exchangeratesapi.io/latest?base=${nuevaMonedaBase}`;
+						$(".precio").remove();
+						$(".moneda").remove();
+						$("#lista-monedas").html("");
+						$("#nueva-moneda").val("");
+						cargarAPI();
+						respuestaJSON.rates = "";
+						console.log("moneda validó correctamente");
+						break;
+					}
 				}
 			}
-			$("#boton-cambiar-moneda").on("click", monedaSeleccionada);
+			$("#boton-cambiar-moneda").on("click", cambiarMonedaBase);
 		});
-	});
+}
+
+cargarAPI();
