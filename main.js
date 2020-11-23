5e7a460df2f165f81747efdbd13960093dc206bc
joaquinluis <joaquin_akerman@hotmail.com>
2020-11-23 16:56:19 -0300

    Refactorizacion del código
    Sepación en bloques. Falta solucionar que el cambio de moneda sea funcional

--- main.js
+++ main.js
@@ -3,42 +3,56 @@
 ///<reference types="jquery"/>
 let paginaAPI = "https://api.exchangeratesapi.io/latest";
 
-function cargarAPI() {
+function mostrarDiaYMoneda(dia, moneda) {
+	$("#fecha").text(`En el día ${dia}`);
+	$(`#descripcion`).text(`1 ${moneda} es igual a:`);
+}
+
+function armarTablaDeCambios(monedas, precio) {
+	for (let i = 0; i < monedas.length; i++) {
+		$("#moneda").append(
+			`<li class="list-group-item moneda">${monedas[i]}</li>`
+		);
+		$("#precio").append(
+			`<li class="list-group-item precio">${precio[i]}</li>`
+		);
+		$("#lista-monedas").append(`<option value="${monedas[i]}">`);
+	}
+}
+
+function cambiarMonedaBase(monedasValidas) {
+	let nuevaMonedaBase = $("#nueva-moneda").val();
+	console.log("me hicieron click");
+	for (let i = 0; i < monedasValidas.length; i++) {
+		if (nuevaMonedaBase === monedasValidas[i]) {
+			paginaAPI = `https://api.exchangeratesapi.io/latest?base=${nuevaMonedaBase}`;
+			$(".precio").remove();
+			$(".moneda").remove();
+			$("#lista-monedas").html("");
+			$("#nueva-moneda").val("");
+			cargarMonedas();
+			respuestaJSON.rates = "";
+			console.log("moneda validó correctamente");
+			break;
+		}
+	}
+}
+
+function cargarMonedas() {
 	fetch(paginaAPI)
 		.then((respuesta) => respuesta.json())
 		.then((respuestaJSON) => {
-			$("#fecha").text(`En el día ${respuestaJSON.date}`);
-			$(`#descripcion`).text(`1 ${respuestaJSON.base} es igual a:`);
-
-			Object.keys(respuestaJSON.rates).forEach((moneda) => {
-				$("#precio").append(
-					`<li class="list-group-item precio">${respuestaJSON.rates[moneda]}</li>`
-				);
-				$("#moneda").append(
-					`<li class="list-group-item moneda">${moneda}</li>`
-				);
-				$("#lista-monedas").append(`<option value="${moneda}">`);
-			});
-
-			function cambiarMonedaBase() {
-				let monedasValidas = Object.keys(respuestaJSON.rates);
-				let nuevaMonedaBase = $("#nueva-moneda").val();
-				for (let i = 0; i < monedasValidas.length; i++) {
-					if (nuevaMonedaBase === monedasValidas[i]) {
-						paginaAPI = `https://api.exchangeratesapi.io/latest?base=${nuevaMonedaBase}`;
-						$(".precio").remove();
-						$(".moneda").remove();
-						$("#lista-monedas").html("");
-						$("#nueva-moneda").val("");
-						cargarAPI();
-						respuestaJSON.rates = "";
-						console.log("moneda validó correctamente");
-						break;
-					}
-				}
-			}
-			$("#boton-cambiar-moneda").on("click", cambiarMonedaBase);
+			mostrarDiaYMoneda(respuestaJSON.date, respuestaJSON.base);
+			armarTablaDeCambios(
+				Object.keys(respuestaJSON.rates),
+				Object.values(respuestaJSON.rates)
+			);
+			$("#boton-cambiar-moneda").on(
+				"click",
+				cambiarMonedaBase(respuestaJSON.rates)
+			);
 		})
+
 		.catch((error) =>
 			console.error(
 				"falló cargar la tabla, intente más nuevamente",
@@ -47,4 +61,4 @@ function cargarAPI() {
 		);
 }
 
-cargarAPI();
+cargarMonedas();
